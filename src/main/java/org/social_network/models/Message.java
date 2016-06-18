40455af3d8899ca796265dcc10fad8b836cf2abe
Evgeny Astafjev <evgenyjdk@gmail.com>
2016-06-18 12:17:52 +0300

    added Hibernate mapping

--- src/main/java/org/social_network/models/Message.java
+++ src/main/java/org/social_network/models/Message.java
@@ -7,7 +7,47 @@ import java.util.Set;
  */
 public class Message {
 
-    private int id;
-    private Set<Profile> profiles;
-    private boolean check;
+    private int messageId;
+    private String messageBody;
+    private Set<User> usersSenders;
+    private Set<User> usersReceivers;
+    private boolean checkStatus;
+
+
+    public Message() {
+    }
+
+    public Message(int messageId, String messageBody, Set<User> usersSenders, Set<User> usersReceivers, boolean checkStatus) {
+        setMessageId(messageId);
+        setMessageBody(messageBody);
+        setUsersSenders(usersSenders);
+        setUsersReceivers(usersReceivers);
+        setCheckStatus(checkStatus);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "messageId=" + messageId +
+                ", usersSenders=" + usersSenders +
+                ", checkStatus=" + checkStatus +
+                '}';
+    }
+
+
+    public int getMessageId() { return messageId; }
+    public void setMessageId(int messageId) { this.messageId = messageId; }
+
+    public String getMessageBody() { return messageBody; }
+    public void setMessageBody(String messageBody) { this.messageBody = messageBody; }
+
+    public Set<User> getUsersReceivers() { return usersReceivers; }
+    public void setUsersReceivers(Set<User> usersReceivers) { this.usersReceivers = usersReceivers; }
+
+    public Set<User> getUsersSenders() { return usersSenders; }
+    public void setUsersSenders(Set<User> usersSenders) { this.usersSenders = usersSenders; }
+
+    public boolean isCheckStatus() { return checkStatus; }
+    public void setCheckStatus(boolean checkStatus) { this.checkStatus = checkStatus;  }
 }
--- src/main/java/org/social_network/models/User.java
+++ src/main/java/org/social_network/models/User.java
@@ -1,34 +1,30 @@
 package org.social_network.models;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by Админ on 07.06.2016.
  */
-public class Profile {
+public class User {
 
-    private  int idProfile;
+    private  int userId;
     private String firstName;
     private String lastName;
     private Date birthday;
-//    private LocalDate birthday;
     private String email;
     private String login;
     private String password;
     private Role role;
-    private Set<Friend> friends;
     private List<Message> messages;
 
-    public Profile() {
+
+    public User() {
     }
 
-    public Profile(int idProfile, String firstName, String lastName, Date birthday, String email, String login,
-                   String password, Role role, Set<Friend> friends, List<Message> messages) {
-        setIdProfile(idProfile);
+    public User(int userId, String firstName, String lastName, Date birthday, String email, String login,
+                String password, Role role, List<Message> messages)      {
+        setUserId(userId);
         setFirstName(firstName);
         setLastName(lastName);
         setBirthday(birthday);
@@ -36,28 +32,12 @@ public class Profile {
         setLogin(login);
         setPassword(password);
         setRole(role);
-        setFriends(friends);
         setMessages(messages);
     }
 
-    @Override
-    public String toString() {
-        return "Profile{" +
-                "idProfile=" + idProfile +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", birthday=" + birthday +
-                ", email='" + email + '\'' +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", role=" + role +
-                ", friends=" + friends +
-                ", messages=" + messages +
-                '}';
-    }
 
-    public int getIdProfile() { return idProfile;  }
-    public void setIdProfile(int idProfile) { this.idProfile = idProfile; }
+    public int getUserId() { return userId;  }
+    public void setUserId(int userId) { this.userId = userId; }
 
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
@@ -80,11 +60,25 @@ public class Profile {
     public Role getRole() { return role; }
     public void setRole(Role role) { this.role = role; }
 
-    public Set<Friend> getFriends() { return friends; }
-    public void setFriends(Set<Friend> friends) { this.friends = friends; }
-
     public List<Message> getMessages() { return messages; }
     public void setMessages(List<Message> messages) { this.messages = messages; }
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthday=" + birthday +
+                ", email='" + email + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", messages=" + messages +
+    }
+
 
 
 }
